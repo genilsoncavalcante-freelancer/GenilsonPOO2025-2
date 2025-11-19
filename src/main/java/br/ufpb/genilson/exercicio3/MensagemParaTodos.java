1a8d905141c2c69873093df1bf9050091a26983e
genilsoncavalcante-freelancer/ <cavalcantegenilson2@gmail.com>
2025-11-18 22:19:46 -0300

    create class MensagemParaTodos.java

---
+++ src/main/java/br/ufpb/genilson/exercicio3/MensagemParaTodos.java
@@ -0,0 +1,17 @@
+package br.ufpb.genilson.exercicio3;
+
+public class MensagemParaTodos extends Mensagem {
+    public MensagemParaTodos(String texto, String emailRemetente, boolean anonima) {
+        super(texto, emailRemetente, anonima);
+    }
+
+    @Override
+    public String getTextoCompletoAExibir() {
+        if (ehAnonima()) {
+            return "Mensagem para todos. Texto: " + getTexto();
+        } else {
+            return "Mensagem de " + getEmailRemetente() +
+                    " para todos. Texto: " + getTexto();
+        }
+    }
+}
